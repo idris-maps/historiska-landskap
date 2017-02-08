d5621a0f7d686b137cad5c4a5ec5224e222dd99d
Idris <idris.maps@gmx.ch>
2017-02-08 12:14:57 +0100

    timeline move

--- main.js
+++ main.js
@@ -1,9 +1,17 @@
 var EventEmitter = require('events').EventEmitter
 var evt = new EventEmitter()
 var dispatch = require('./lib/dispatch')
+var Timeline = require('./lib/Timeline')
 
 window.onload = function() {
-	dispatch(evt)
+	var tl = new Timeline(evt)
+	var o = {
+		evt: evt,
+		timeline: tl
+	}
+	window.o = o
+	dispatch(o)
+
 	evt.emit('resize')
 }
 
