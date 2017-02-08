f0e8bbf5fbf3c8e988c7f459bea6c023ad207682
Idris <idris.maps@gmx.ch>
2017-02-08 10:48:51 +0100

    first

---
+++ make.js
@@ -0,0 +1,14 @@
+var fs = require('fs')
+var html = require('./make/make-html')
+
+var htmlConfig = {
+	keywords: '',
+	description: '',
+	title: '',
+	style: ['style.css'],
+	script: []
+}
+
+html(htmlConfig, function(page) {
+	fs.writeFile('public/index.html', page, 'utf-8', function() { console.log('wrote public/index.html') })
+})
---
+++ make/make-html.js
@@ -0,0 +1,34 @@
+var fs = require('fs')
+var map = fs.readFileSync('./svg/map.svg', 'utf-8')
+var tl = fs.readFileSync('./svg/timeline.svg', 'utf-8')
+
+function html(o) { 
+	var str = '<!doctype html><html>'
+	+ '<head><meta charset="utf-8">'
+	+ '<meta name="viewport" content="width=device-width, initial-scale=1"/>'
+	+ '<meta name="keywords" content="' + o.keywords + '"/>'
+	+ '<meta name="description" content="' + o.description + '"/>'
+	+ '<title>' + o.title + '</title>'
+
+	o.style.forEach(function(s) {
+		str = str 	+ '<link rel="stylesheet" href="' + s + '">'
+	})
+
+	str = str + '</head><body>'
+	+ o.body
+	
+	o.script.forEach(function(s) {
+		str = str + 	+ '<script src="' + s + '"></script>'
+	})
+
+	str = str + '</body></html>'
+
+	return str
+}
+
+module.exports = function(o, callback) {
+	o.body = '<div id="div-map">' + map + '</div>'
+	+ '<div id="div-timeline">' + tl + '</div>'
+	var page = html(o)
+	callback(page)
+}
